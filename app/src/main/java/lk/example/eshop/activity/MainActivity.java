6ee4892a0f6ca85bbf5b1659bfd6a5b12b7d355c
Sanjana Nisal Kumara <146843659+sanjana-kumara@users.noreply.github.com>
2026-02-25 13:15:00 +0530

    create fragments for Side nave bar Bottom nave bar and sync it

--- app/src/main/java/lk/example/eshop/activity/MainActivity.java
+++ app/src/main/java/lk/example/eshop/activity/MainActivity.java
@@ -1,26 +1,172 @@
 package lk.example.eshop.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+import fragment.CartFragment;
+import fragment.CategoryFragment;
+import fragment.HomeFragment;
+import fragment.MessageFragment;
+import fragment.OrdersFragment;
+import fragment.ProfileFragment;
+import fragment.SettingFragment;
+import fragment.WishlistFragment;
 import lk.example.eshop.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, NavigationBarView.OnItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private MaterialToolbar toolbar ;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
+
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.side_navigation_view);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true){
+            @Override
+            public void handleOnBackPressed(){
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    finish();
+                }
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainContent), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        if (savedInstanceState == null){
+            loadFragment(new HomeFragment());
+        }
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+
+        int itemId = item.getItemId();
+
+        navigationView.setCheckedItem(-1);
+
+        if (itemId == R.id.side_nav_home || itemId == R.id.bottom_nav_home) {
+            loadFragment(new HomeFragment());
+//            navigationView.setCheckedItem(R.id.side_nav_home);
+            navigationView.getMenu().findItem(R.id.side_nav_home).setChecked(true);
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_home).setChecked(true);
+
+        }else if (itemId == R.id.side_nav_profile || itemId == R.id.bottom_nav_profile) {
+            loadFragment(new ProfileFragment());
+//            navigationView.setCheckedItem(R.id.side_nav_profile);
+            navigationView.getMenu().findItem(R.id.side_nav_profile).setChecked(true);
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_profile).setChecked(true);
+
+        } else if (itemId == R.id.side_nav_orders) {
+            loadFragment(new OrdersFragment());
+//            navigationView.setCheckedItem(R.id.side_nav_orders);
+            navigationView.getMenu().findItem(R.id.side_nav_orders).setChecked(true);
+
+
+        } else if (itemId == R.id.side_nav_wishlist) {
+            loadFragment(new WishlistFragment());
+            navigationView.getMenu().findItem(R.id.side_nav_wishlist).setChecked(true);
+
+
+        }else if (itemId == R.id.side_nav_cart || itemId == R.id.bottom_nav_cart) {
+            loadFragment(new CartFragment());
+            navigationView.getMenu().findItem(R.id.side_nav_cart).setChecked(true);
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_cart).setChecked(true);
+
+        }else if (itemId == R.id.side_nav_message) {
+            loadFragment(new MessageFragment());
+            navigationView.getMenu().findItem(R.id.side_nav_message).setChecked(true);
+
+        } else if (itemId == R.id.side_nav_setting) {
+            loadFragment(new SettingFragment());
+            navigationView.getMenu().findItem(R.id.side_nav_setting).setChecked(true);
+
+        } else if (itemId == R.id.bottom_nav_category) {
+            loadFragment(new CategoryFragment());
+            bottomNavigationView.getMenu().findItem(R.id.bottom_nav_category).setChecked(true);
+
+        } else if (itemId == R.id.side_nav_login) {
+
+        } else if (itemId == R.id.side_nav_logout) {
+
+        }
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        return true;
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+    }
+
 }
